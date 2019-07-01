5b5a4f8d8efe7560cfd4dd5163b4bd6825b8c629
Tess Neau <tessneau@gmail.com>
2019-07-01 11:57:40 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your code here
+
+//
+// let configObj = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(formData)
+// };
+//
+// fetch("http://localhost:3000/dogs", configObj);
+
+// function IdToDOM(id) {
+//   const p = document.createElement('p')
+//   p.innerHTML = id
+//   document.body.append(p)
+// }
+//
+// let formData = {
+//   name: userName,
+//   email: userEmail
+// };
+
+
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  }).then(res => res.json())
+  .then(data => document.body.innerHTML = data["id"])
+  .catch(error => document.body.innerHTML = error.message)
+}
